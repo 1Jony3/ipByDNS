203c751ce6e67df1371ac7ae708a5360fc13db70
Jane <ezhbikovskaya@gmail.com>
2021-11-25 21:14:24 +0700

    Search ip by dns(finish protection from fools)

--- app/src/main/java/com/example/ipbydns/MainActivity.java
+++ app/src/main/java/com/example/ipbydns/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.ipbydns;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +18,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void Search(View view) {
+        /*try {
+            TextInputEditText textInputEditText = findViewById(R.id.text);
+            String domain = textInputEditText.getText().toString();
+            InetAddress giriAddress = InetAddress.getByName("ipv4.local.com");
+            String addresess = giriAddress.getHostAddress();
+            Toast toast = Toast.makeText(this, addresess, Toast.LENGTH_LONG);
+            toast.show();
+        } catch (Exception e) {
+            e.getMessage();
+        }*/
+
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try  {TextInputEditText textInputEditText = findViewById(R.id.text);
+                    String host = textInputEditText.getText().toString();
+                    InetAddress giriAddress = InetAddress.getByName(host);
+                    String addresess = giriAddress.getHostAddress();
+                    textInputEditText.setText(addresess);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+
+    }
+
 }
