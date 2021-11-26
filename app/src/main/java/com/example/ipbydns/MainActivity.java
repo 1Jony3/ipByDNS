7b8c8d7f6a5ab5cb85cdc5ebbe774e574e48602b
Jane <ezhbikovskaya@gmail.com>
2021-11-26 21:00:58 +0700

    finished

--- app/src/main/java/com/example/ipbydns/MainActivity.java
+++ app/src/main/java/com/example/ipbydns/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -18,35 +20,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void Search(View view) {
-        /*try {
-            TextInputEditText textInputEditText = findViewById(R.id.text);
-            String domain = textInputEditText.getText().toString();
-            InetAddress giriAddress = InetAddress.getByName("ipv4.local.com");
-            String addresess = giriAddress.getHostAddress();
-            Toast toast = Toast.makeText(this, addresess, Toast.LENGTH_LONG);
-            toast.show();
-        } catch (Exception e) {
-            e.getMessage();
-        }*/
+        TextInputEditText textInputEditText = findViewById(R.id.text);
+        String domainName = textInputEditText.getText().toString();
+        domainName = domainName.replaceFirst("\\s+", "");
+        domainName = domainName.toLowerCase();
+        TextInputLayout layout = findViewById(R.id.textInputLayout);
 
-        Thread thread = new Thread(new Runnable() {
+        layout.setErrorEnabled(false);
+        if (domainName.equals("")) {
+            layout.setError("empty");
+            layout.setErrorEnabled(true);
+        } else {
+            String finalDomainName = domainName;
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        InetAddress giriAddress = InetAddress.getByName(finalDomainName);
+                        String addresess = giriAddress.getHostAddress();
 
-            @Override
-            public void run() {
-                try  {TextInputEditText textInputEditText = findViewById(R.id.text);
-                    String host = textInputEditText.getText().toString();
-                    InetAddress giriAddress = InetAddress.getByName(host);
-                    String addresess = giriAddress.getHostAddress();
-                    textInputEditText.setText(addresess);
-                } catch (Exception e) {
-                    e.printStackTrace();
+                        TextView textView = findViewById(R.id.textView);
+                        String text = "ip: " + addresess;
+                        textView.setText(text);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-        });
-
-        thread.start();
-
+            });
+            thread.start();
+        }
     }
 
 }
